820668d7b63ad72eb9e40ce6e87a6bd0a08b248a
Bernard Bussy <brbitzbussy@gmail.com>
2019-02-09 09:07:47 +0200

    added admin email

--- party/company/company.go
+++ party/company/company.go
@@ -1,6 +1,10 @@
 package company
 
 type Company struct {
-	Id string `json:"id" bson:"id"`
-	Name string `json:"name" bson:"name"`
+	Id         string `json:"id" bson:"id"`
+	Name       string `json:"name" bson:"name"`
+	// The email address which will be used to invite the admin
+	// user of the company
+	// I.e. the first user of the system from the company
+	AdminEmail string `json:"adminEmail" bson:"adminEmail"`
 }
