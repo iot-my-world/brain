457d5d4652d1998776e60d7bde1764d433b98dcf
Bernard Bussy <brbitzbussy@gmail.com>
2019-02-26 14:48:43 +0200

    system entity and basic recordHandler added

---
+++ party/system/recordHandler/adaptor/jsonRpc/adaptor.go
@@ -0,0 +1,155 @@
+package user
+
+import (
+	"gitlab.com/iotTracker/brain/api"
+	"gitlab.com/iotTracker/brain/party/user"
+	userRecordHandler "gitlab.com/iotTracker/brain/party/user/recordHandler"
+	"gitlab.com/iotTracker/brain/search/wrappedIdentifier"
+	"gitlab.com/iotTracker/brain/validate/reasonInvalid"
+	"net/http"
+)
+
+type adaptor struct {
+	RecordHandler userRecordHandler.RecordHandler
+}
+
+func New(recordHandler userRecordHandler.RecordHandler) *adaptor {
+	return &adaptor{
+		RecordHandler: recordHandler,
+	}
+}
+
+type CreateRequest struct {
+	User user.User `json:"user"`
+}
+
+type CreateResponse struct {
+	User user.User `json:"user"`
+}
+
+func (s *adaptor) Create(r *http.Request, request *CreateRequest, response *CreateResponse) error {
+	createUserResponse := userRecordHandler.CreateResponse{}
+
+	if err := s.RecordHandler.Create(
+		&userRecordHandler.CreateRequest{
+			User: request.User,
+		},
+		&createUserResponse); err != nil {
+		return err
+	}
+
+	response.User = createUserResponse.User
+
+	return nil
+}
+
+type RetrieveRequest struct {
+	Identifier wrappedIdentifier.WrappedIdentifier `json:"identifier"`
+}
+
+type RetrieveResponse struct {
+	User user.User `json:"user" bson:"user"`
+}
+
+func (s *adaptor) Retrieve(r *http.Request, request *RetrieveRequest, response *RetrieveResponse) error {
+	id, err := request.Identifier.UnWrap()
+	if err != nil {
+		return err
+	}
+
+	retrieveUserResponse := userRecordHandler.RetrieveResponse{}
+	if err := s.RecordHandler.Retrieve(
+		&userRecordHandler.RetrieveRequest{
+			Identifier: id,
+		},
+		&retrieveUserResponse); err != nil {
+		return err
+	}
+
+	response.User = retrieveUserResponse.User
+
+	return nil
+}
+
+type UpdateRequest struct {
+	Identifier wrappedIdentifier.WrappedIdentifier `json:"identifier"`
+	User       user.User                           `json:"user"`
+}
+
+type UpdateResponse struct {
+	User user.User `json:"user"`
+}
+
+func (s *adaptor) Update(r *http.Request, request *UpdateRequest, response *UpdateResponse) error {
+	id, err := request.Identifier.UnWrap()
+	if err != nil {
+		return err
+	}
+
+	updateUserResponse := userRecordHandler.UpdateResponse{}
+	if err := s.RecordHandler.Update(
+		&userRecordHandler.UpdateRequest{
+			Identifier: id,
+		},
+		&updateUserResponse); err != nil {
+		return err
+	}
+
+	response.User = updateUserResponse.User
+
+	return nil
+}
+
+type DeleteRequest struct {
+	Identifier wrappedIdentifier.WrappedIdentifier `json:"identifier"`
+}
+
+type DeleteResponse struct {
+	User user.User `json:"user"`
+}
+
+func (s *adaptor) Delete(r *http.Request, request *DeleteRequest, response *DeleteResponse) error {
+	id, err := request.Identifier.UnWrap()
+	if err != nil {
+		return err
+	}
+
+	deleteUserResponse := userRecordHandler.DeleteResponse{}
+	if err := s.RecordHandler.Delete(
+		&userRecordHandler.DeleteRequest{
+			Identifier: id,
+		},
+		&deleteUserResponse); err != nil {
+		return err
+	}
+
+	response.User = deleteUserResponse.User
+
+	return nil
+}
+
+type ValidateRequest struct {
+	User   user.User  `json:"user"`
+	Method api.Method `json:"method"`
+}
+
+type ValidateResponse struct {
+	ReasonsInvalid []reasonInvalid.ReasonInvalid `json:"reasonsInvalid"`
+}
+
+func (s *adaptor) Validate(r *http.Request, request *ValidateRequest, response *ValidateResponse) error {
+
+	validateUserResponse := userRecordHandler.ValidateResponse{}
+	if err := s.RecordHandler.Validate(
+		&userRecordHandler.ValidateRequest{
+			User:   request.User,
+			Method: request.Method,
+		},
+		&validateUserResponse); err != nil {
+		return err
+	}
+
+	response.ReasonsInvalid = validateUserResponse.ReasonsInvalid
+
+	return nil
+}
---
+++ party/system/recordHandler/mongo/recordHandler.go
@@ -0,0 +1,482 @@
+package mongo
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	brainException "gitlab.com/iotTracker/brain/exception"
+	"gitlab.com/iotTracker/brain/log"
+	"gitlab.com/iotTracker/brain/party/user"
+	userException "gitlab.com/iotTracker/brain/party/user/exception"
+	userRecordHandler "gitlab.com/iotTracker/brain/party/user/recordHandler"
+	userSetup "gitlab.com/iotTracker/brain/party/user/setup"
+	"gitlab.com/iotTracker/brain/search/identifier/emailAddress"
+	"gitlab.com/iotTracker/brain/search/identifier/id"
+	"gitlab.com/iotTracker/brain/search/identifier/username"
+	"gitlab.com/iotTracker/brain/validate/reasonInvalid"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2"
+)
+
+type mongoRecordHandler struct {
+	mongoSession         *mgo.Session
+	database             string
+	collection           string
+	createIgnoredReasons reasonInvalid.IgnoredReasonsInvalid
+}
+
+func New(
+	mongoSession *mgo.Session,
+	database,
+	collection,
+	rootPasswordFileLocation string,
+) *mongoRecordHandler {
+
+	setupIndices(mongoSession, database, collection)
+
+	createIgnoredReasons := reasonInvalid.IgnoredReasonsInvalid{
+		ReasonsInvalid: map[string][]reasonInvalid.Type{
+			"id": {
+				reasonInvalid.Blank,
+			},
+			"password": {
+				reasonInvalid.Blank,
+			},
+		},
+	}
+
+	newUserMongoRecordHandler := mongoRecordHandler{
+		mongoSession:         mongoSession,
+		database:             database,
+		collection:           collection,
+		createIgnoredReasons: createIgnoredReasons,
+	}
+
+	if err := userSetup.InitialSetup(&newUserMongoRecordHandler, rootPasswordFileLocation); err != nil {
+		log.Fatal("Unable to complete initial user setup!", err.Error())
+	}
+
+	return &newUserMongoRecordHandler
+}
+
+func setupIndices(mongoSession *mgo.Session, database, collection string) {
+	//Initialise User collection in database
+	mgoSesh := mongoSession.Copy()
+	defer mgoSesh.Close()
+	userCollection := mgoSesh.DB(database).C(collection)
+
+	// Ensure id uniqueness
+	idUnique := mgo.Index{
+		Key:    []string{"id"},
+		Unique: true,
+	}
+	if err := userCollection.EnsureIndex(idUnique); err != nil {
+		log.Fatal("Could not ensure id uniqueness: ", err)
+	}
+
+	//Ensure username Uniqueness
+	usernameUnique := mgo.Index{
+		Key:    []string{"username"},
+		Unique: true,
+	}
+	if err := userCollection.EnsureIndex(usernameUnique); err != nil {
+		log.Fatal("Could not ensure username uniqueness: ", err)
+	}
+
+	//Ensure emailAddress Uniqueness
+	emailAddressUnique := mgo.Index{
+		Key:    []string{"emailAddress"},
+		Unique: true,
+	}
+	if err := userCollection.EnsureIndex(emailAddressUnique); err != nil {
+		log.Fatal("Could not ensure email address uniqueness: ", err)
+	}
+}
+
+func (mrh *mongoRecordHandler) ValidateCreateRequest(request *userRecordHandler.CreateRequest) error {
+	reasonsInvalid := make([]string, 0)
+
+	// Validate the new user
+	userValidateResponse := userRecordHandler.ValidateResponse{}
+
+	err := mrh.Validate(&userRecordHandler.ValidateRequest{User: request.User}, &userValidateResponse)
+	if err != nil {
+		reasonsInvalid = append(reasonsInvalid, "unable to validate newUser")
+	} else {
+		for _, reason := range userValidateResponse.ReasonsInvalid {
+			if !mrh.createIgnoredReasons.CanIgnore(reason) {
+				reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("%s - %s", reason.Field, reason.Type))
+			}
+		}
+	}
+
+	if len(reasonsInvalid) > 0 {
+		return brainException.RequestInvalid{Reasons: reasonsInvalid}
+	} else {
+		return nil
+	}
+}
+
+func (mrh *mongoRecordHandler) Create(request *userRecordHandler.CreateRequest, response *userRecordHandler.CreateResponse) error {
+	if err := mrh.ValidateCreateRequest(request); err != nil {
+		return err
+	}
+
+	mgoSession := mrh.mongoSession.Copy()
+	defer mgoSession.Close()
+
+	userCollection := mgoSession.DB(mrh.database).C(mrh.collection)
+
+	newId, err := uuid.NewV4()
+	if err != nil {
+		return brainException.UUIDGeneration{Reasons: []string{err.Error()}}
+	}
+	request.User.Id = newId.String()
+
+	if err := userCollection.Insert(request.User); err != nil {
+		return userException.Create{Reasons: []string{"inserting record", err.Error()}}
+	}
+
+	response.User = request.User
+	return nil
+}
+
+func (mrh *mongoRecordHandler) ValidateRetrieveRequest(request *userRecordHandler.RetrieveRequest) error {
+	reasonsInvalid := make([]string, 0)
+
+	if request.Identifier == nil {
+		reasonsInvalid = append(reasonsInvalid, "identifier is nil")
+	} else {
+		if !user.IsValidIdentifier(request.Identifier) {
+			reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("identifier of type %s not supported for user", request.Identifier.Type()))
+		}
+	}
+
+	if len(reasonsInvalid) > 0 {
+		return brainException.RequestInvalid{Reasons: reasonsInvalid}
+	} else {
+		return nil
+	}
+}
+
+func (mrh *mongoRecordHandler) Retrieve(request *userRecordHandler.RetrieveRequest, response *userRecordHandler.RetrieveResponse) error {
+	if err := mrh.ValidateRetrieveRequest(request); err != nil {
+		return err
+	}
+
+	mgoSession := mrh.mongoSession.Copy()
+	defer mgoSession.Close()
+
+	userCollection := mgoSession.DB(mrh.database).C(mrh.collection)
+
+	var userRecord user.User
+
+	filter := request.Identifier.ToFilter()
+	if err := userCollection.Find(filter).One(&userRecord); err != nil {
+		if err == mgo.ErrNotFound {
+			return userException.NotFound{}
+		} else {
+			return brainException.Unexpected{Reasons: []string{err.Error()}}
+		}
+	}
+
+	response.User = userRecord
+	return nil
+}
+
+func (mrh *mongoRecordHandler) ValidateUpdateRequest(request *userRecordHandler.UpdateRequest) error {
+	reasonsInvalid := make([]string, 0)
+
+	if len(reasonsInvalid) > 0 {
+		return brainException.RequestInvalid{Reasons: reasonsInvalid}
+	} else {
+		return nil
+	}
+}
+
+func (mrh *mongoRecordHandler) Update(request *userRecordHandler.UpdateRequest, response *userRecordHandler.UpdateResponse) error {
+	if err := mrh.ValidateUpdateRequest(request); err != nil {
+		return err
+	}
+
+	mgoSession := mrh.mongoSession.Copy()
+	defer mgoSession.Close()
+
+	userCollection := mgoSession.DB(mrh.database).C(mrh.collection)
+
+	// Retrieve User
+	retrieveUserResponse := userRecordHandler.RetrieveResponse{}
+	if err := mrh.Retrieve(&userRecordHandler.RetrieveRequest{Identifier: request.Identifier}, &retrieveUserResponse); err != nil {
+		return userException.Update{Reasons: []string{"retrieving record", err.Error()}}
+	}
+
+	// Update fields:
+	// retrieveUserResponse.User.Id = request.User.Id // cannot update ever
+	retrieveUserResponse.User.Name = request.User.Name
+	retrieveUserResponse.User.Surname = request.User.Surname
+	// retrieveUserResponse.User.Username = request.User.Username // cannot update yet
+	// retrieveUserResponse.User.EmailAddress = request.User.EmailAddress // cannot update yet
+	retrieveUserResponse.User.Password = request.User.Password
+	retrieveUserResponse.User.Roles = request.User.Roles
+	// retrieveUserResponse.User.PartyType = request.User.PartyType // cannot update yet
+	// retrieveUserResponse.User.PartyId = request.User.PartyId // cannot update yet
+
+	if err := userCollection.Update(request.Identifier.ToFilter(), retrieveUserResponse.User); err != nil {
+		return userException.Update{Reasons: []string{"updating record", err.Error()}}
+	}
+
+	response.User = retrieveUserResponse.User
+
+	return nil
+}
+
+func (mrh *mongoRecordHandler) ValidateDeleteRequest(request *userRecordHandler.DeleteRequest) error {
+	reasonsInvalid := make([]string, 0)
+
+	if request.Identifier == nil {
+		reasonsInvalid = append(reasonsInvalid, "identifier is nil")
+	} else {
+		if !user.IsValidIdentifier(request.Identifier) {
+			reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("identifier of type %s not supported for user", request.Identifier.Type()))
+		}
+	}
+
+	if len(reasonsInvalid) > 0 {
+		return brainException.RequestInvalid{Reasons: reasonsInvalid}
+	} else {
+		return nil
+	}
+}
+
+func (mrh *mongoRecordHandler) Delete(request *userRecordHandler.DeleteRequest, response *userRecordHandler.DeleteResponse) error {
+	if err := mrh.ValidateDeleteRequest(request); err != nil {
+		return err
+	}
+
+	mgoSession := mrh.mongoSession.Copy()
+	defer mgoSession.Close()
+
+	userCollection := mgoSession.DB(mrh.database).C(mrh.collection)
+
+	if err := userCollection.Remove(request.Identifier.ToFilter()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (mrh *mongoRecordHandler) ValidateValidateRequest(request *userRecordHandler.ValidateRequest) error {
+	reasonsInvalid := make([]string, 0)
+
+	if len(reasonsInvalid) > 0 {
+		return brainException.RequestInvalid{Reasons: reasonsInvalid}
+	} else {
+		return nil
+	}
+}
+
+func (mrh *mongoRecordHandler) Validate(request *userRecordHandler.ValidateRequest, response *userRecordHandler.ValidateResponse) error {
+	if err := mrh.ValidateValidateRequest(request); err != nil {
+		return err
+	}
+
+	allReasonsInvalid := make([]reasonInvalid.ReasonInvalid, 0)
+	userToValidate := &request.User
+
+	if (*userToValidate).Id == "" {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "id",
+			Type:  reasonInvalid.Blank,
+			Help:  "id cannot be blank",
+			Data:  (*userToValidate).Id,
+		})
+	}
+
+	if (*userToValidate).Name == "" {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "name",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).Name,
+		})
+	}
+
+	if (*userToValidate).Surname == "" {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "surname",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).Name,
+		})
+	}
+
+	if (*userToValidate).Username == "" {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "username",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).Username,
+		})
+	}
+
+	if (*userToValidate).EmailAddress == "" {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "emailAddress",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).EmailAddress,
+		})
+	}
+
+	if len((*userToValidate).Password) == 0 {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "password",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).Password,
+		})
+	}
+
+	if (*userToValidate).PartyType == "" {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "partyType",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).PartyType,
+		})
+	}
+
+	blankIdIdentifier := id.Identifier{}
+	if (*userToValidate).PartyId == blankIdIdentifier {
+		allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+			Field: "partyId",
+			Type:  reasonInvalid.Blank,
+			Help:  "cannot be blank",
+			Data:  (*userToValidate).PartyId,
+		})
+	}
+
+	returnedReasonsInvalid := make([]reasonInvalid.ReasonInvalid, 0)
+
+	// Perform additional checks/ignores considering method field
+	switch request.Method {
+	case userRecordHandler.Create:
+		// Check if the users email already exists by checking if a user can be
+		// retrieved with it
+		if (*userToValidate).EmailAddress != "" {
+			if err := mrh.Retrieve(&userRecordHandler.RetrieveRequest{
+				Identifier: emailAddress.Identifier{
+					EmailAddress: (*userToValidate).EmailAddress,
+				},
+			},
+				&userRecordHandler.RetrieveResponse{}); err != nil {
+				switch err.(type) {
+				case userException.NotFound:
+					// this is what we want, do nothing
+				default:
+					allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+						Field: "emailAddress",
+						Type:  reasonInvalid.Unknown,
+						Help:  "unknown error",
+						Data:  (*userToValidate).EmailAddress,
+					})
+				}
+			} else {
+				// there was no error, this email address is already taken by another user
+				allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+					Field: "emailAddress",
+					Type:  reasonInvalid.Duplicate,
+					Help:  "already exists",
+					Data:  (*userToValidate).EmailAddress,
+				})
+			}
+		}
+
+		// Check if the users username already exists by checking if a user can be
+		// retrieved with it
+		if (*userToValidate).EmailAddress != "" {
+			if err := mrh.Retrieve(&userRecordHandler.RetrieveRequest{
+				Identifier: username.Identifier{
+					Username: (*userToValidate).Username,
+				},
+			},
+				&userRecordHandler.RetrieveResponse{}); err != nil {
+				switch err.(type) {
+				case userException.NotFound:
+					// this is what we want, do nothing
+				default:
+					allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+						Field: "username",
+						Type:  reasonInvalid.Unknown,
+						Help:  "unknown error",
+						Data:  (*userToValidate).Username,
+					})
+				}
+			} else {
+				// there was no error, this username is already taken by another user
+				allReasonsInvalid = append(allReasonsInvalid, reasonInvalid.ReasonInvalid{
+					Field: "username",
+					Type:  reasonInvalid.Duplicate,
+					Help:  "already exists",
+					Data:  (*userToValidate).Username,
+				})
+			}
+		}
+
+		// Ignore reasons not applicable for this method
+		for _, reason := range allReasonsInvalid {
+			if !mrh.createIgnoredReasons.CanIgnore(reason) {
+				returnedReasonsInvalid = append(returnedReasonsInvalid, reason)
+			}
+		}
+
+	default:
+		returnedReasonsInvalid = allReasonsInvalid
+	}
+
+	response.ReasonsInvalid = returnedReasonsInvalid
+	return nil
+}
+
+func (mrh *mongoRecordHandler) ValidateChangePasswordRequest(request *userRecordHandler.ChangePasswordRequest) error {
+	reasonsInvalid := make([]string, 0)
+
+	if len(reasonsInvalid) > 0 {
+		return brainException.RequestInvalid{Reasons: reasonsInvalid}
+	} else {
+		return nil
+	}
+}
+
+func (mrh *mongoRecordHandler) ChangePassword(request *userRecordHandler.ChangePasswordRequest, response *userRecordHandler.ChangePasswordResponse) error {
+	if err := mrh.ValidateChangePasswordRequest(request); err != nil {
+		return err
+	}
+
+	// Retrieve User
+	retrieveUserResponse := userRecordHandler.RetrieveResponse{}
+	if err := mrh.Retrieve(&userRecordHandler.RetrieveRequest{Identifier: request.Identifier}, &retrieveUserResponse); err != nil {
+		return userException.ChangePassword{Reasons: []string{"retrieving record", err.Error()}}
+	}
+
+	// Hash the new Password
+	pwdHash, err := bcrypt.GenerateFromPassword([]byte(request.NewPassword), bcrypt.DefaultCost)
+	if err != nil {
+		return userException.ChangePassword{Reasons: []string{"hashing password", err.Error()}}
+	}
+
+	mgoSession := mrh.mongoSession.Copy()
+	defer mgoSession.Close()
+
+	userCollection := mgoSession.DB(mrh.database).C(mrh.collection)
+
+	// update user
+	retrieveUserResponse.User.Password = pwdHash
+
+	if err := userCollection.Update(request.Identifier.ToFilter(), retrieveUserResponse.User); err != nil {
+		return userException.Update{Reasons: []string{"updating record", err.Error()}}
+	}
+
+	response.User = retrieveUserResponse.User
+
+	return nil
+}
