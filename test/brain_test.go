b5395c52a9905f6f8e5bab1b922913a21d680c4f
Bernard Bussy <brbitzbussy@gmail.com>
2019-03-08 22:19:48 +0200

    client test starting

--- test/brain_test.go
+++ test/brain_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/suite"
 	systemTest "gitlab.com/iotTracker/brain/test/party/system"
 	companyTest "gitlab.com/iotTracker/brain/test/party/company"
+	clientTest "gitlab.com/iotTracker/brain/test/party/client"
 )
 
 // In order for 'go test' to run this suite, we need to create
@@ -12,4 +13,5 @@ import (
 func TestBrain(t *testing.T) {
 	suite.Run(t, new(systemTest.System))
 	suite.Run(t, new(companyTest.Company))
+	suite.Run(t, new(clientTest.Client))
 }
--- test/party/client/test.go
+++ test/party/client/test.go
@@ -4,6 +4,8 @@ import (
 	"github.com/stretchr/testify/suite"
 	jsonRpcClient "gitlab.com/iotTracker/brain/communication/jsonRpc/client"
 	basicJsonRpcClient "gitlab.com/iotTracker/brain/communication/jsonRpc/client/basic"
+	authJsonRpcAdaptor "gitlab.com/iotTracker/brain/security/auth/service/adaptor/jsonRpc"
+	"fmt"
 )
 
 type Client struct {
@@ -16,6 +18,25 @@ func (suite *Client) SetupTest() {
 	suite.jsonRpcClient = basicJsonRpcClient.New("http://localhost:9010/api")
 }
 
-func (suite *Client) TestLogin() {
+func (suite *Client) TestClientInviteAndRegisterUsers() {
+	for companyOwner := range EntitiesAndAdminUsersToCreate {
+		for clientDataEntityIdx := range EntitiesAndAdminUsersToCreate[companyOwner] {
+			clientDataEntity := &EntitiesAndAdminUsersToCreate[companyOwner][clientDataEntityIdx]
+			// log in the client
+			if err := suite.jsonRpcClient.Login(authJsonRpcAdaptor.LoginRequest{
+				UsernameOrEmailAddress: clientDataEntity.AdminUser.Username,
+				Password:               string(clientDataEntity.AdminUser.Password),
+			}); err != nil {
+				suite.FailNow(fmt.Sprintf("failed to log in as %s", clientDataEntity.AdminUser.Username), err.Error())
+			}
 
+			//// invite and register all of the users
+			//for userIdx := range (*clientDataEntity).Users {
+			//
+			//}
+
+			// log out
+			suite.jsonRpcClient.Logout()
+		}
+	}
 }
