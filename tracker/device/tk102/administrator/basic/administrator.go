1fb2498ea127d59a52cf24aa409e63cddd7c13d6
Bernard Bussy <brbitzbussy@gmail.com>
2019-03-13 11:15:43 +0200

    code to update the device owner and asssigned party type aded

--- tracker/device/tk102/administrator/basic/administrator.go
+++ tracker/device/tk102/administrator/basic/administrator.go
@@ -59,22 +59,33 @@ func (ba *basicAdministrator) ValidateChangeOwnerRequest(request *tk102DeviceAdm
 				reasonsInvalid = append(reasonsInvalid, fmt.Sprintf("device invalid: %s - %s - %s", reason.Field, reason.Type, reason.Help))
 			}
 		} else {
-			// retrieve the owner and assigned parties to check the relationship is valid
-			ownerPartyRetrieveResponse := partyHandler.RetrievePartyResponse{}
-			if err := ba.partyHandler.RetrieveParty(&partyHandler.RetrievePartyRequest{
-				Claims:     request.Claims,
-				Identifier: request.TK102.OwnerId,
-				PartyType:  request.TK102.OwnerPartyType,
-			}, &ownerPartyRetrieveResponse); err != nil {
-				reasonsInvalid = append(reasonsInvalid, "error retrieving owner party: "+err.Error())
-			}
-			assignedPartyRetrieveResponse := partyHandler.RetrievePartyResponse{}
-			if err := ba.partyHandler.RetrieveParty(&partyHandler.RetrievePartyRequest{
-				Claims:     request.Claims,
-				Identifier: request.TK102.AssignedId,
-				PartyType:  request.TK102.AssignedPartyType,
-			}, &assignedPartyRetrieveResponse); err != nil {
-				reasonsInvalid = append(reasonsInvalid, "error retrieving assigned party: "+err.Error())
+			// if the assigned party is set OR
+			if request.TK102.AssignedId.Id != "" ||
+				// if the owner and assigned parties are not the same
+				request.TK102.AssignedId.Id != request.TK102.OwnerId.Id {
+				// then we must retrieve the owner and assigned parties to check the relationship is valid
+				ownerPartyRetrieveResponse := partyHandler.RetrievePartyResponse{}
+				if err := ba.partyHandler.RetrieveParty(&partyHandler.RetrievePartyRequest{
+					Claims:     request.Claims,
+					Identifier: request.TK102.OwnerId,
+					PartyType:  request.TK102.OwnerPartyType,
+				}, &ownerPartyRetrieveResponse); err != nil {
+					reasonsInvalid = append(reasonsInvalid, "error retrieving owner party: "+err.Error())
+				}
+				assignedPartyRetrieveResponse := partyHandler.RetrievePartyResponse{}
+				if err := ba.partyHandler.RetrieveParty(&partyHandler.RetrievePartyRequest{
+					Claims:     request.Claims,
+					Identifier: request.TK102.AssignedId,
+					PartyType:  request.TK102.AssignedPartyType,
+				}, &assignedPartyRetrieveResponse); err != nil {
+					reasonsInvalid = append(reasonsInvalid, "error retrieving assigned party: "+err.Error())
+				}
+
+				// the owner party must be the parent party of the assigned party
+				if ownerPartyRetrieveResponse.Party.Details().PartyType != assignedPartyRetrieveResponse.Party.Details().ParentPartyType ||
+					ownerPartyRetrieveResponse.Party.Details().PartyId != assignedPartyRetrieveResponse.Party.Details().ParentId {
+					reasonsInvalid = append(reasonsInvalid, "owner party must be the parent party of the assigned party")
+				}
 			}
 		}
 	}
@@ -117,7 +128,21 @@ func (ba *basicAdministrator) ChangeOwnershipAndAssignment(request *tk102DeviceA
 		},
 		// Query: blank query as we have no restriction
 	}, &readingRetrieveResponse); err != nil {
-		return brainException.Unexpected{Reasons: []string{"collecting readings", err.Error()}}
+		return tk102DeviceAdministratorException.ReadingCollection{Reasons: []string{err.Error()}}
+	}
+
+	// 3. update the device
+	tk102RetrieveResponse.TK102.OwnerPartyType = request.TK102.OwnerPartyType
+	tk102RetrieveResponse.TK102.OwnerId = request.TK102.OwnerId
+	tk102RetrieveResponse.TK102.AssignedPartyType = request.TK102.AssignedPartyType
+	tk102RetrieveResponse.TK102.AssignedId = request.TK102.AssignedId
+	tk102UpdateResponse := tk102RecordHandler.UpdateResponse{}
+	if err := ba.tk102RecordHandler.Update(&tk102RecordHandler.UpdateRequest{
+		Claims:     request.Claims,
+		Identifier: id.Identifier{Id: request.TK102.Id},
+		TK102:      tk102RetrieveResponse.TK102,
+	}, &tk102UpdateResponse); err != nil {
+		return tk102DeviceAdministratorException.DeviceUpdate{Reasons: []string{err.Error()}}
 	}
 
 	// 4. update the readings
