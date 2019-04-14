5b0cfbb36eef3e24d01f6fd5261d4f107fc79255
Bernard Bussy <brbitzbussy@gmail.com>
2019-04-14 14:18:12 +0200

    added some basic validation

--- tracker/device/validator/basic/validator.go
+++ tracker/device/validator/basic/validator.go
@@ -21,6 +21,18 @@ func New(
 func (v *validator) ValidateValidateRequest(request *deviceValidator.ValidateRequest) error {
 	reasonsInvalid := make([]string, 0)
 
+	if request.Device == nil {
+		reasonsInvalid = append(reasonsInvalid, "device is nil")
+	} else {
+		if v.deviceValidators[request.Device.Type()] == nil {
+			reasonsInvalid = append(reasonsInvalid, "no validator for this device type")
+		}
+	}
+
+	if request.Claims == nil {
+		reasonsInvalid = append(reasonsInvalid, "claims are nil")
+	}
+
 	if len(reasonsInvalid) > 0 {
 		return brainException.RequestInvalid{Reasons: reasonsInvalid}
 	}
@@ -31,6 +43,5 @@ func (v *validator) Validate(request *deviceValidator.ValidateRequest) (*deviceV
 	if err := v.ValidateValidateRequest(request); err != nil {
 		return nil, err
 	}
-
-	return &deviceValidator.ValidateResponse{}, nil
+	return v.deviceValidators[request.Device.Type()].Validate(request)
 }
