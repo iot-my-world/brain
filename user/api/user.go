3c49156e856a2a3f92a5c0c7f4c64ca88b9821bf
Bernard Bussy <brbitzbussy@gmail.com>
2019-04-17 22:50:24 +0200

    starting api user

--- user/api/user.go
+++ user/api/user.go
@@ -1,1 +1,23 @@
 package api
+
+import (
+	"gitlab.com/iotTracker/brain/party"
+	"gitlab.com/iotTracker/brain/search/identifier/id"
+)
+
+type User struct {
+	Id string `json:"id" bson:"id"`
+
+	// Details
+	Name        string `json:"name" bson:"name"`
+	Description string `json:"description" bson:"description"`
+
+	// System Details
+	Username string   `json:"username" bson:"username"`
+	Password []byte   `json:"password" bson:"password"`
+	Roles    []string `json:"roles" bson:"roles"`
+
+	// Party Details
+	PartyType party.Type    `json:"parentPartyType" bson:"parentPartyType"`
+	PartyId   id.Identifier `json:"partyId" bson:"partyId"`
+}
