393a3edc1a0259d15997a72c604251efb5d81feb
Bernard Bussy <brbitzbussy@gmail.com>
2019-05-23 20:41:42 +0200

    register device administrator adaptor with api user server

--- start/brain.go
+++ start/brain.go
@@ -607,6 +607,9 @@ func main() {
 	}
 
 	// ZX303 Device
+	if err := secureAPIUserAPIServer.RegisterService(ZX303DeviceAdministratorAdaptor, "ZX303DeviceAdministrator"); err != nil {
+		log.Fatal("Unable to Register API User ZX303 Device Administrator Service Adaptor")
+	}
 	if err := secureAPIUserAPIServer.RegisterService(ZX303DeviceAuthenticatorAdaptor, "ZX303DeviceAuthenticator"); err != nil {
 		log.Fatal("Unable to Register API User ZX303 Device Authenticator Service Adaptor")
 	}
