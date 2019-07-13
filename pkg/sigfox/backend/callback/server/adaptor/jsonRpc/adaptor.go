f5a5efa90df4beddc8a6e7a31b51919a71197ee5
Bernard Bussy <brbitzbussy@gmail.com>
2019-07-13 15:34:50 +0200

    sigfox backend callback server adaptor

--- pkg/sigfox/backend/callback/server/adaptor/jsonRpc/adaptor.go
+++ pkg/sigfox/backend/callback/server/adaptor/jsonRpc/adaptor.go
@@ -1,1 +1,40 @@
 package jsonRpc
+
+import (
+	sigfoxBackendCallbackDataMessage "github.com/iot-my-world/brain/pkg/sigfox/backend/callback/data/message"
+	sigfoxBackendCallbackServer "github.com/iot-my-world/brain/pkg/sigfox/backend/callback/server"
+	"net/http"
+)
+
+type adaptor struct {
+	Server sigfoxBackendCallbackServer.Server
+}
+
+func New(
+	Server sigfoxBackendCallbackServer.Server,
+) *adaptor {
+	return &adaptor{
+		Server: Server,
+	}
+}
+
+type HandleDataMessageRequest struct {
+	Device string `json:"device"`
+	Data   []byte `json:"data"`
+}
+
+type HandleDataMessageResponse struct {
+}
+
+func (a *adaptor) HandleDataMessage(r *http.Request, request *HandleDataMessageRequest, response *HandleDataMessageResponse) error {
+	if _, err := a.Server.HandleDataMessage(&sigfoxBackendCallbackServer.HandleDataMessageRequest{
+		Message: sigfoxBackendCallbackDataMessage.Message{
+			DeviceId: request.Device,
+			Data:     request.Data,
+		},
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
