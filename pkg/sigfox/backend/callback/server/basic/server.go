250633467f6118c2893daa1fe86f9da404c94263
Bernard Bussy <brbitzbussy@gmail.com>
2019-07-15 21:17:06 +0200

    add message handling in sigfox backend basic callback server

--- pkg/sigfox/backend/callback/server/basic/server.go
+++ pkg/sigfox/backend/callback/server/basic/server.go
@@ -1,22 +1,44 @@
 package basic
 
 import (
+	"github.com/iot-my-world/brain/internal/log"
 	sigfoxBackendDataMessageHandler "github.com/iot-my-world/brain/pkg/sigfox/backend/callback/data/message/handler"
 	sigfoxBackendCallbackServer "github.com/iot-my-world/brain/pkg/sigfox/backend/callback/server"
+	sigfoxBackendCallbackServerException "github.com/iot-my-world/brain/pkg/sigfox/backend/callback/server/exception"
 )
 
 type server struct {
 	handlers []sigfoxBackendDataMessageHandler.Handler
 }
 
-func New() sigfoxBackendCallbackServer.Server {
-	return &server{}
+func New(
+	handlers []sigfoxBackendDataMessageHandler.Handler,
+) sigfoxBackendCallbackServer.Server {
+	return &server{
+		handlers: handlers,
+	}
 }
 
 func (s *server) MethodRequiresAuthorization(string) bool {
 	return true
 }
 
-func (s *server) HandleDataMessage(*sigfoxBackendCallbackServer.HandleDataMessageRequest) (*sigfoxBackendCallbackServer.HandleDataMessageResponse, error) {
+func (s *server) HandleDataMessage(request *sigfoxBackendCallbackServer.HandleDataMessageRequest) (*sigfoxBackendCallbackServer.HandleDataMessageResponse, error) {
+	for handlerIdx := range s.handlers {
+		wantsMessage, err := s.handlers[handlerIdx].WantMessage(request.Message)
+		if err != nil {
+			err = sigfoxBackendCallbackServerException.HandleDataMessage{Reasons: []string{"determining if handler wants message", err.Error()}}
+			log.Error(err.Error())
+			return nil, err
+		}
+		if wantsMessage {
+			err := s.handlers[handlerIdx].Handle(request.Message)
+			if err != nil {
+				err = sigfoxBackendCallbackServerException.HandleDataMessage{Reasons: []string{"handling message", err.Error()}}
+				log.Error(err.Error())
+				return nil, err
+			}
+		}
+	}
 	return nil, nil
 }
