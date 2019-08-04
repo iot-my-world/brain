07fda7c93bc34b449e2ed211cf3f6243ec7f3bd4
Bernard Bussy <brbitzbussy@gmail.com>
2019-08-04 21:06:21 +0200

    add timestamp to message structure

--- pkg/sigfox/backend/callback/data/message/message.go
+++ pkg/sigfox/backend/callback/data/message/message.go
@@ -1,9 +1,10 @@
 package message
 
 type Message struct {
-	Id       string `json:"id" bson:"id"`
-	DeviceId string `json:"deviceId" bson:"deviceId"`
-	Data     []byte `json:"data" bson:"data"`
+	Id        string `json:"id" bson:"id"`
+	Timestamp int    `json:"timeStamp" bson:"timeStamp"`
+	DeviceId  string `json:"deviceId" bson:"deviceId"`
+	Data      []byte `json:"data" bson:"data"`
 }
 
 func (m *Message) SetId(id string) {
